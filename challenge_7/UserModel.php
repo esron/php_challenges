9f31575a43c581a0cba718c3b5ab877e398550eb
Esron <esron.dtamar@gmail.com>
2021-05-16 16:19:12 -0300

    cria user model

---
+++ challenge_7/UserModel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace UsersAPI;
+
+class UserModel
+{
+    public string $id;
+    public string $firstName;
+    public string $lastName;
+    public string $email;
+    public string $phone;
+
+    public function __construct(string $firstName, string $lastName, string $email, string $phone)
+    {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->email = $email;
+        $this->phone = $phone;
+    }
+}
