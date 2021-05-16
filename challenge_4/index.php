f3e79b8289c2db66ab5d0394d1cd179f869baf47
Esron <esron.dtamar@gmail.com>
2021-05-15 21:25:23 -0300

    adicona validação de email já utilizado

--- challenge_4/index.php
+++ challenge_4/index.php
@@ -1,30 +1,62 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $emailIndex = 2;
+    $phoneIndex = 3;
 
-$firstName = filter_input(INPUT_POST, 'first_name', FILTER_SANITIZE_STRING);
-$lastName = filter_input(INPUT_POST, 'last_name', FILTER_SANITIZE_STRING);
-$formEmail = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-$formPhone = filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_STRING);
+    $firstName = filter_input(INPUT_POST, 'first_name', FILTER_SANITIZE_STRING);
+    $lastName = filter_input(INPUT_POST, 'last_name', FILTER_SANITIZE_STRING);
+    $formEmail = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    $formPhone = filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_STRING);
+    $login = filter_input(INPUT_POST, 'login', FILTER_SANITIZE_STRING);
+    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
 
-// Email validation
-$email = filter_var($formEmail, FILTER_VALIDATE_EMAIL);
-if ($formEmail === '') {
-    $formError['email'] = 'Email é obrigatório';
-} elseif ($email === false) {
-    $formError['email'] = 'Email inválido';
-}
+    // Email validation
+    $email = filter_var($formEmail, FILTER_VALIDATE_EMAIL);
+    if ($formEmail === '') {
+        $formError['email'] = 'Email é obrigatório';
+    } elseif ($email === false) {
+        $formError['email'] = 'Email inválido';
+    } else {
+        $handle = fopen('registros.txt', 'r');
 
-// Phone validation
-$phoneLen = strlen($formPhone);
-if ($formPhone !== ''
-    && (!is_numeric($formPhone)
-        || $phoneLen > 11
-        || $phoneLen < 10)) {
-    $formError['phone'] = 'Telefone inválido';
-    $phone = false;
-}
+        while (($line = fgetcsv($handle, 1000)) !== false) {
+            $fileEmail = $line[$emailIndex];
 
-if ($phone !== false && $email !== false) {
+            if ($email === $fileEmail) {
+                $formError['email'] = 'Este email já esta em uso, tente outro';
+                break;
+            }
+        }
 
+        fclose($handle);
+    }
+
+    // Phone validation
+    $phoneLen = strlen($formPhone);
+    $phone = true;
+    if ($formPhone !== ''
+        && (!is_numeric($formPhone)
+            || $phoneLen > 11
+            || $phoneLen < 10)) {
+        $formError['phone'] = 'Telefone inválido';
+        $phone = false;
+    }
+
+    if (isset($formError['email']) === false
+        && isset($formError['phone']) === false
+        && ($handle = fopen('registros.txt', 'a+'))
+    ) {
+        fputcsv($handle, [
+            $firstName,
+            $lastName,
+            $email,
+            $phone,
+            $login,
+            password_hash($password, PASSWORD_BCRYPT),
+        ]);
+
+        fclose($handle);
+    }
 }
 
 ?>
@@ -43,7 +75,7 @@ if ($phone !== false && $email !== false) {
     <br>
 
     <label for="email">Email</label>
-    <input type="text" id="email" name="email" placeholder="name@example.com" value="<?= htmlentities($email === false ? $formEmail ?? '' : '') ?>">
+    <input type="text" id="email" name="email" placeholder="name@example.com" value="<?= htmlentities(isset($formError['email']) ? $formEmail ?? '' : '') ?>">
     <?php if (isset($formError['email'])) {
         echo sprintf('<p>%s</p>', htmlentities($formError['email']));
     } ?>
@@ -51,7 +83,7 @@ if ($phone !== false && $email !== false) {
     <br>
 
     <label for="email">Telefone</label>
-    <input type="text" id="phone" name="phone" placeholder="XX99999999" value="<?= htmlentities($phone === false ? $formPhone ?? '' : '') ?>">
+    <input type="text" id="phone" name="phone" placeholder="XX99999999" value="<?= htmlentities(isset($formError['phone']) ? $formPhone ?? '' : '') ?>">
     <?php if (isset($formError['phone'])) {
         echo sprintf('<p>%s</p>', htmlentities($formError['phone']));
     } ?>
