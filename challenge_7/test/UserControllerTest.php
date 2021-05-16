cf634308c3a2b1e82e98c47a1c589d1c9fe438c6
Esron <esron.dtamar@gmail.com>
2021-05-16 19:45:15 -0300

    testa o getUsers do UserController

---
+++ challenge_7/test/UserControllerTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test;
+
+use PHPUnit\Framework\TestCase;
+use UsersAPI\UserController;
+use UsersAPI\UserRepository;
+use UsersAPI\UserModel;
+
+class UserControllerTest extends TestCase
+{
+    public function testItReturnsUsers()
+    {
+        $returnUsers = [
+            new UserModel(
+                'Esron',
+                'Silva',
+                'esron.dtamar@gmail.com',
+                '74999999999'
+            ),
+            new UserModel(
+                'Silva',
+                'Esron',
+                'silva.esron@gmail.com',
+                '87900000000'
+            ),
+        ];
+
+        $userRepositoryMock = $this->createMock(UserRepository::class);
+        $userRepositoryMock->method('getUsers')
+            ->willReturn($returnUsers);
+
+        $userController = new UserController($userRepositoryMock);
+
+        $this->assertEquals(json_encode([
+            'data' => [
+                [
+                    'firstName' => 'Esron',
+                    'lastName' => 'Silva',
+                    'email' => 'esron.dtamar@gmail.com',
+                    'phone' => '74999999999',
+                ],
+                [
+                    'firstName' => 'Silva',
+                    'lastName' => 'Esron',
+                    'email' => 'silva.esron@gmail.com',
+                    'phone' => '87900000000',
+                ],
+            ]
+        ]), $userController->getUsers());
+    }
+}
